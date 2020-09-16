452795033bce2c74f93bc6b904948bd8a86cd498
WAIYARAUNG.17 <WAIYARAUNG.17@ichat.sp.edu.sg>
2020-09-17 00:44:25 +0800

    Use FutureBuilder to build ListView

--- lib/listpage.dart
+++ lib/listpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easyentry/db/db_helper.dart';
+import 'package:easyentry/model/location.dart';
 
 class Listpage extends StatefulWidget {
   @override
@@ -13,17 +14,47 @@ class _ListpageState extends State<Listpage> {
   Widget build(BuildContext context) {
     return Container(
         height: 44.0,
-        child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                  height: 50,
-                  child: Center(
-                    child:
-                        Text('Test ${DbHelper.retrieveLocations().toString()}'),
-                  ));
-            }));
+        child: FutureBuilder(
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Location>> snapshot) {
+            //TODO: show proper loading screen
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Text("Error");
+              case ConnectionState.waiting:
+                return Text("Loading...");
+              case ConnectionState.active:
+                return Text("Loading...");
+              case ConnectionState.done:
+                return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          height: 50,
+                          child: Center(
+                            child: Text('${snapshot.data[index].getTitle()}'),
+                          ));
+                    });
+            }
+            return null; // unreachable
+          },
+          future: DbHelper.retrieveLocations(),
+        ));
   }
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //       height: 44.0,
+  //       child: ListView.builder(
+  //           padding: const EdgeInsets.all(8),
+  //           itemCount: 10,
+  //           itemBuilder: (BuildContext context, int index) {
+  //             return Container(
+  //                 height: 50,
+  //                 child: Center(
+  //                   child:
+  //                       Text('Test ${DbHelper.retrieveLocations().toString()}'),
+  //                 ));
+  //           }));
+  // }
 }
-//await DbHelper.retrieveLocations()).toString()
